368946fccc95a687d925d7f10da23ff3e4692c3e
MUhammad Jalloh <dev@TechBuktu.com>
2018-05-13 16:12:27 -0400

    Implemented segmented settings package to handle dev and prod erenvironments

---
+++ blogmatiq/settings/dev.py
@@ -0,0 +1,12 @@
+from blogmatiq.settings.main import * 
+
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+STATIC_ROOT = ""
+MEDIA_ROOT = ""
---
+++ blogmatiq/settings/prod.py
@@ -0,0 +1,11 @@
+from blogmatiq.settings.main import * 
+
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', 'SERVER_IP', ]
+
+STATIC_ROOT = ""
+MEDIA_ROOT = ""
