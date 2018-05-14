f97862c79a9aeaf9b3f7373b1ef42a560b0be088
MUhammad Jalloh <dev@TechBuktu.com>
2018-05-14 03:31:54 -0400

    Additional implementaion of BloggerSerializer

--- blogger/api/serializers.py
+++ blogger/api/serializers.py
@@ -2,13 +2,21 @@ from rest_framework import serializers
 from blogger.models import (Blogger, Blog, BlogCategory, BlogPost)
 
 class BloggerSerializer(serializers.HyperlinkedModelSerializer):
-    pass 
+    """
+        BloggerSerializer is the API serializer for the blogger.Blogger model
+    """
+    user = serializers.HyperlinkedRelatedField(
+        queryset= User.objectd.all(),
+        view_name = 'socialite_api:user_detail' ,
+        lookup_field = "link"
+    )
 
 class BlogSerializer(serializers.HyperlinkedModelSerializer):
     pass 
 
 class BlogCategorySeriaizer(serializers.HyperlinkedModelSerializer):
     pass 
+
 
 class BlogPostSerializer(serializers.HyperlinkedModelSerializer):
     pass 
