542401536d7040e8ea505e7cf0a3050d90ee97b0
Muhammad Jalloh <dev@TechBuktu.com>
2018-05-17 17:58:48 -0400

    Added the Blogger app to the blogmatiq.urls definitions

--- blogmatiq/urls.py
+++ blogmatiq/urls.py
@@ -20,5 +20,6 @@ from socialite.api.views import blogmatiq_api_root
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/blogger/', include('blogger.api.urls', namespace="blogger_api")),
-    url(r'^$', blogmatiq_api_root, name="blogmatiq_api_root"),
+    url(r'^api/$', blogmatiq_api_root, name="blogmatiq_api_root"),
+    url(r'', include('blogger.urls', namespace="blogger")),
 ]
