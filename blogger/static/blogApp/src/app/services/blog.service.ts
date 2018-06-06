d9765fc3ff418743fb96db1aa34ddb83950e67e1
Muhammad Jalloh <dev@TechBuktu.com>
2018-06-06 19:25:47 -0400

    Created the AuthService to handle authentication for the app's users

--- blogger/static/blogApp/src/app/services/blog.service.ts
+++ blogger/static/blogApp/src/app/services/blog.service.ts
@@ -5,10 +5,33 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class BlogService {
-
   bloApiUrl:string = "/api/blogs/";
 
   constructor(private Http: HttpClient) { }
 
+  getAllBlogs(){
+   return this.http.get(this.blogApiUrl);
+  }
+
+  getBlogByUrl(blog_url){
+    return this.http.get(this.blogApiUrl + blog_url);
+  }
+
+  createBlog(new_blog_data){
+    return this.http.post(this.blogApiUrl, new_blog_data);
+  }
+
+  updateBlog(blog_url, updated_blog_data){
+    return this.http.put(this.blogApiUrl, blog_url, updated_blog_data);
+  }
+
+  deleteBlog(blog_url){
+    return this.http.delete(this.blogApiUrl + blog_url);
+  }
+
+  getBlogsByBlogger(bloggerUrl){
+    return this.http.get(this.blogApiUrl + '?blogger__link=' + bloggerUrl);
+  }
+  
 
 }
