9f73df2b5c5d00c34e8957db824baa506036919f
Muhammad Jalloh <dev@TechBuktu.com>
2018-06-07 17:26:41 -0400

    Created ModelAdmin classes for the blogger app's models and registered them with AdminSite

--- blogger/admin.py
+++ blogger/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from blogger.models import (
+    Blogger, Blog, BlogCategory, BlogPost, Comment
+)
 
 # Register your models here.
+class BloggerAdmin(admin.ModelAdmin):
+    fields = []
+
+class BlogAdmin(admin.ModelAdmin):
+    fields = []
+
+class BlogCategoryAdmin(admin.ModelAdmin):
+    fields = []
+
+class BlogPostAdmin(admin.ModelAdmin):
+    fields = []
+
+class CommentAdmin(admin.ModelAdmin):
+    fields = []
+
+admin.site.register(Blogger, BloggerAdmin)
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(BlogCategory, BlogCategoryAdmin)
+admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(Comment, CommentAdmin)
